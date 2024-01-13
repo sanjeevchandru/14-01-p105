3910c25b3535438205bb1234732aeb58f660646f
sanjeevchandru <chandrusanjeev5@gmail.com>
2024-01-14 00:04:38 +0530

    14-01-p105

---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("69.to compute the sum of three elements of a given array of integers of length 3."+"<br>");
+function test69(arr){
+    return arr[0]+arr[1]+arr[2];
+}
+document.write("The array :[1,2,3],, Ans :"+test69([1,2,3])+"<br><br>");
